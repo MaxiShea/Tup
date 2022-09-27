8fe18ec1dbf18e338d2a27dfa16f8c511478ab49
MaxiShea <88351855+MaxiShea@users.noreply.github.com>
2022-09-27 20:43:57 -0300

    Add files via upload

---
+++ Python/PreParcial/practica1.py
@@ -0,0 +1,36 @@
+# Definir una función primeros_tres(lista) que reciba como parámetro la siguiente lista y retorne 
+# una nueva lista con los primeros 3 elementos.
+# lista = ["ho", 3.16, "la", 81, 6, 42]
+
+
+def primeros_tres(lista):
+    nuevalista = []
+    nuevalista = lista[:3]
+    return nuevalista
+
+lista = ["ho", 3.16, "la", 81, 6, 42]
+ListaNueva = primeros_tres(lista)
+print(ListaNueva)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+   
+
+
+
+
